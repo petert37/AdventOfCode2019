7171ef36e5f30cdfe794b46b27717d6e171d4750
Tóth Péter <toth.peter37@gmail.com>
2019-12-13 17:39:30 +0100

    day1

--- src/day1/day1.kt
+++ src/day1/day1.kt
@@ -1,5 +1,17 @@
 package day1
 
+import java.io.File
+import kotlin.math.floor
+import kotlin.math.max
+
 fun main() {
-    print("Hello world")
+    val input = File("src/day1/input.txt").readLines()
+    println("Total fuel: ${input.map { getFuel(it.toInt()) }.sum()}")
+    println("Total fuel recursive: ${input.map { getTotalFuel(it.toInt()) }.sum()}")
+}
+
+fun getFuel(mass: Int) = max(0, floor(mass / 3.0).toInt() - 2)
+fun getTotalFuel(mass: Int): Int {
+    val fuel = getFuel(mass)
+    return if (fuel == 0) 0 else fuel + getTotalFuel(fuel)
 }
